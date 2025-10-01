489432c483ea6c4426f0287556328befcb385a55
Edi <ediajiatas@gmail.com>
2025-10-01 13:34:56 -0700

    Half way done with itemized bill

--- question2.py
+++ question2.py
@@ -1,35 +1,42 @@
 # Question 2: Restaurant Order System (Lesson 2 - If/Elif/Else)
 print("=== RESTAURANT MENU === \n 1. Pizza - $15.99 \n 2. Burger - $12.50 \n 3. Salad - $9.99 \n 4. Pasta - $13.75")
 
-Choice = int(input("Enter your choice (1-4): "))
-drink = input("Would you like a drink? ($2.50) (yes/no): ")
-    # yes = "Drink: Yes - $2.50"
-    # no = "Drink: No"
-    # item = "Food:"
+choice = int(input("Enter your choice (1-4): "))
+drink_choice = input("Would you like a drink? ($2.50) (yes/no): ")
+
+pizza = 15.99
+burger = 12.50
+salad = 9.99
+pasta = 13.75
+
+yes_drink = 2.50
+no_drink = 0
+
+if choice == 1:
+    food_price = pizza
+    print(f"Pizza - ${pizza}")
     
-
-if Choice == 1 and drink == "yes":
-        print("=== YOUR ORDER === \n Food: Pizza - $15.99 \n Drink: Yes - $2.50 \n Subtotal: $18.49 \n Tax (8%): $1.48 \n Total: $19.97")
-        
-#     elif Choice == 2:
-#         print(f"{item} Burger - $12.50")
-        
-#     elif Choice == 3:
-#         print(f"{item} Salad - $9.99")
-        
-#     elif Choice == 4:
-#         print(f"{item} Pasta - $13.75")
-
-#     else:
-#         print("Invalid Response. Please use a number between 1-4.")
-
-#     if drink == "yes":
-#         print("Drink: Yes - $2.50")
-        
-#     elif drink == "no":
-#         print("Drink: No $0")
-        
-# except ValueError:
-#     print("Please enter valid numbers")
+elif choice == 2:
+    food_price = burger
+    print(f"Burger - ${burger}")
     
-# if Choice == 1
+elif choice == 3:
+    food_price = salad
+    print(f"Salad - ${salad}")
+
+elif choice == 4:
+    food_price = pasta
+    print(f"Pasta: - ${pasta}")
+    
+if drink_choice == "yes":
+    drink_price = yes_drink
+    print(f"Drink: Yes - ${yes_drink}")
+
+elif drink_choice == "no":
+    drink_price = no_drink
+    print(f"Drink: No - ${no_drink}")
+    
+subtotal = food_price + drink_price
+print(f"Subtotal: ${subtotal:.2f}")
+    
+    
