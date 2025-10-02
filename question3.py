dd3ac91aef6ccfbc746a19b1d0d40f65002f251a
Edi <ediajiatas@gmail.com>
2025-10-01 22:52:13 -0700

    finished question 3

--- question3.py
+++ question3.py
@@ -1,20 +1,39 @@
 # Question 3: Pet Age Calculator 
-pet = input("Enter pet type (dog/cat/bird/hamster): ")
-human_age = int(input("Enter your pet's age in human years: "))
+while True:
+    pet = input("Enter pet type (dog/cat/bird/hamster): ").lower()
+    if pet in ["dog", "cat", "bird", "hamster"]:
+        break
+    else: 
+        print("Invalid pet type. Please enter dog, cat, bird, or hamster")
+
+while True:
+    human_age = int(input("Enter your pet's age in human years: "))
+    if human_age < 0:
+        print("Age cannot be negative! Please enter a valid number.")
+    else:
+        break
+
 
 print("=== PET AGE CONVERSION ===")
 print(f"Pet Type: {pet}")
+print(f"Human Age: {human_age} years")
 
 if pet == "dog" or pet == "cat":
-    0 > human_age <= 2
-    print(f"Human Age: {human_age} years")
-    print(f"Pet Age: {human_age * 12}")
-
+    if human_age <= 2:
+        pet_age = 24
+    else:
+        pet_age = 24 + (human_age -2) * 4
     
-elif pet == "dog" or pet == "cat":
-    human_age > 3 
-    print(f"Pet Age: {human_age * 12 + 4}")
+elif pet == "bird":
+    pet_age = human_age * 9
     
-
+elif pet == "hamster":
+    pet_age = human_age * 25
     
-
+else:
+    pet_age = None
+    
+if pet_age is not None:
+    print(f"Pet Age: {pet_age} years")
+    print(f"Fun Fact: Your {pet} is like a {pet_age} year-old human!")
+    
